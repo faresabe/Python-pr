84b2a5d2ee089b0b1cc4caad5f76b7c30919a0ad
faresabe <faresabenet9@gmail.com>
2026-09-25 16:04:57 +0300

    add code

--- advanced/decorter.py
+++ advanced/decorter.py
@@ -1,10 +1,32 @@
+import time
+
+def executiontimer(method):
+    def wrapper(instance):
+        start = time.perf_counter()
+
+        method(instance)
+
+        end = time.perf_counter()
+
+        elapsed = end - start
+
+        print(f"Execution time: {elapsed:.6f} seconds")
+
+    return wrapper
+
+
+
+
 
 class test:
-    
-    def test2():
-        for i in range(1000):
+    @executiontimer
+    def test2(self):
+        for i in range(10000):
             pass
         print("finished")
+
+
+
 
 t = test()
 t.test2()
